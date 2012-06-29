2ca84a61f0e0065d00f70a27b7e18ea7d305c68e
Sam Saccone <sam@mojotech.com>
2012-06-29 09:35:12 -0400

    first

---
+++ fader.js
@@ -0,0 +1,24 @@
+$(function(){
+  var _html     = $('html'); 
+  var _window   = $(window);
+  var _menu     = $('.non_content_wrap');
+  var canCheck  = true;
+      
+  _menu.bind('mouseover', showMenu);
+  _window.bind('scroll', fadeMenu);
+
+  function showMenu() {
+    _menu.stop();
+   _menu.animate({'opacity':1},'fast');
+  }
+
+  function fadeMenu(e) {
+    if (canCheck) {
+        canCheck = false;
+        if (!_menu.is(':animated')) {
+            _menu.animate({'opacity':0.2});
+        }
+        setTimeout(function(){canCheck=true},100);
+    }
+  }  
+});
